49af14f29cb9c58ddd66d2e588edf7bc70e86e3a
iparrales123 <47342722+iparrales123@users.noreply.github.com>
2020-09-18 11:50:42 -0700

    used beautify

--- scripts/main.js
+++ scripts/main.js
@@ -16,65 +16,65 @@ function setDetails(imageUrl, titleText) {
     detailTitle.textContent = titleText;
 }
 
-function imageFromThumb(thumbnail){
+function imageFromThumb(thumbnail) {
     'use strict';
     return thumbnail.getAttribute('data-image-url');
 }
 
-function titleFromThumb(thumbnail){
+function titleFromThumb(thumbnail) {
     'use strict';
     return thumbnail.getAttribute('data-image-title');
 }
 
-function setDetailsFromThumb(thumbnail){
+function setDetailsFromThumb(thumbnail) {
     'use strict';
     setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
-function addThumbClickHandler(thumb){
+function addThumbClickHandler(thumb) {
     'use strict';
-    thumb.addEventListener('click', function(event){
+    thumb.addEventListener('click', function (event) {
         event.preventDefault();
         setDetailsFromThumb(thumb);
         showDetails();
     });
 }
 
-function getThumbnailsArray(){
+function getThumbnailsArray() {
     'use strict';
     var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
     var thumbnailArray = [].slice.call(thumbnails);
     return thumbnailArray;
 }
 
-function hideDetails(){
+function hideDetails() {
     'use strict';
     document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
-function showDetails(){
+function showDetails() {
     'use strict';
     var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
     document.body.classList.remove(HIDDEN_DETAIL_CLASS);
     frame.classList.add(TINY_EFFECT_CLASS);
-    setTimeout(function(){
+    setTimeout(function () {
         frame.classList.remove(TINY_EFFECT_CLASS);
     }, 50);
 }
 
-function addKeyPressHandler(){
+function addKeyPressHandler() {
     'use strict';
-    document.body.addEventListener('keyup', function(event){
+    document.body.addEventListener('keyup', function (event) {
         event.preventDefault();
         console.log(event.keyCode);
-        if (event.keyCode === ESC_KEY){
+        if (event.keyCode === ESC_KEY) {
             hideDetails();
         }
     });
 }
 
 //Function for the arrows
-function arrowButtons(){
+function arrowButtons() {
     'use strict';
     var getTitle = document.querySelector(DETAIL_TITLE_SELECTOR); //get the title of the current title image
     var buttons = document.querySelectorAll(RIGHT_LEFT_BUTTONS); //get the two buttons 
@@ -83,46 +83,44 @@ function arrowButtons(){
     var rightButton = buttonArray[1];
     var currentTitle; //will hold the new current title once arrow is clicked 
     var currentImage; //will hold the new current image once arrow is clicked 
-    var thumbnailsArray = getThumbnailsArray(); 
+    var thumbnailsArray = getThumbnailsArray();
 
-    leftButton.addEventListener('click', function(event){
+    leftButton.addEventListener('click', function (event) {
         event.preventDefault();
-        
+
         //loop through thumbnail array to get index of current image
         //to properly execute the left arrow  
-        for(var i = 0; i < thumbnailsArray.length; i++){
-            if(thumbnailsArray[i].getAttribute("data-image-title") == getTitle.textContent){
-                if(i == 0){ 
+        for (var i = 0; i < thumbnailsArray.length; i++) {
+            if (thumbnailsArray[i].getAttribute("data-image-title") == getTitle.textContent) {
+                if (i == 0) {
                     currentImage = imageFromThumb(thumbnailsArray.slice(-1)); //slice(-1) is to get the last 
                     currentTitle = titleFromThumb(thumbnailsArray.slice(-1)); //element of the array
-                    setDetails(currentImage,currentTitle);
+                    setDetails(currentImage, currentTitle);
                     break;
-                }
-                else{
+                } else {
                     currentImage = imageFromThumb(thumbnailsArray[i - 1]);
                     currentTitle = titleFromThumb(thumbnailsArray[i - 1]);
-                    setDetails(currentImage,currentTitle);
-                }              
+                    setDetails(currentImage, currentTitle);
+                }
             }
         }
     });
-    
+
     //same as above, loop through to find the index to properly execute right arrow
-    rightButton.addEventListener('click', function(event){
+    rightButton.addEventListener('click', function (event) {
         event.preventDefault();
-        for(var i = 0; i < thumbnailsArray.length; i++){
-            if(thumbnailsArray[i].getAttribute("data-image-title") == getTitle.textContent){
-                if(i == thumbnailsArray.length -1){
+        for (var i = 0; i < thumbnailsArray.length; i++) {
+            if (thumbnailsArray[i].getAttribute("data-image-title") == getTitle.textContent) {
+                if (i == thumbnailsArray.length - 1) {
                     currentImage = imageFromThumb(thumbnailsArray[0]);
                     currentTitle = titleFromThumb(thumbnailsArray[0]);
-                    setDetails(currentImage,currentTitle);
-                }
-                else{
+                    setDetails(currentImage, currentTitle);
+                } else {
                     currentImage = imageFromThumb(thumbnailsArray[i + 1]);
                     currentTitle = titleFromThumb(thumbnailsArray[i + 1]);
-                    setDetails(currentImage,currentTitle);
+                    setDetails(currentImage, currentTitle);
                     break;
-                }              
+                }
             }
         }
     });
